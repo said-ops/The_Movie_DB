b1287e2b0a6439c6aca4e66af64e2a5a99ece06f
said-ops <idrissisaid29@gmail.com>
2024-09-24 02:41:29 +0100

    repo init

---
+++ firesbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASbyac-qFUiSNQS1wSUQn4idjOLXLanYM",
+  authDomain: "the-movie-db-98b79.firebaseapp.com",
+  projectId: "the-movie-db-98b79",
+  storageBucket: "the-movie-db-98b79.appspot.com",
+  messagingSenderId: "239147086743",
+  appId: "1:239147086743:web:23d91a46232d04874a8ad2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1>The Movie DB</h1>
+  )
+}
+
+export default App
