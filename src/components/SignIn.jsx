5b4feb4978f716b8f253a3a98cb8326a0f118795
said-ops <idrissisaid29@gmail.com>
2024-09-24 20:20:34 +0100

    sign in page styled

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,16 +3,19 @@ import React from "react";
 function SignIn() {
   return (
     <>
-      <section className="sign-contaienr">
+      <section className="sign-container">
         <form action="#">
           <h1>Sign In</h1>
           <div className="inputs">
             <label htmlFor="email">Email Address</label>
             <input type="text" name="email" id="email" />
+            <span className="error">this field is required</span>
             <label htmlFor="password">Password</label>
             <input type="password" name="password" id="password" />
+            <span className="error">this field is required</span>
           </div>
           <button type="submit">Sign In</button>
+          <span className="option">Already have an account? , <span className="link">Login</span></span>
         </form>
       </section>
     </>
