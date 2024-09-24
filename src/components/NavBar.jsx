b6e15c4758b8f187c1e3480b46ed8347ccfd873c
said-ops <idrissisaid29@gmail.com>
2024-09-24 23:16:20 +0100

    menu completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
 function NavBar() {
+
+  //handle menu
+ const [isOpen,setIsOpen]=useState(false)
+  const handleClick = ()=>{
+    setIsOpen(!isOpen)
+  }
+
+ 
   return (
     <>
         <nav>
             <h1>MovieDB</h1>
-            <ul className='nav-links'>
+            <ul className={`nav-links  ${isOpen ? '' : 'hide-menu'}`}>
             <li>Home</li>
             <li>Find Movie</li>
             <li>Watchlist</li>
             <li>Sign</li>
             </ul>
-            <div className='menu-icon'>
-              <img src="/images/icon-hamburger.svg" alt="menu" />
+            <div  
+            className='menu-icon'
+            onClick={handleClick}
+            >
+              <img 
+              src="/images/icon-hamburger.svg" 
+              alt="menu"
+               />
             </div>
         </nav>
     </>
